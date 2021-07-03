766891ab338f04d1c8239585ba98f98e8f9164ea
Ryalsi12 <86335825+Ryalsi12@users.noreply.github.com>
2021-07-03 09:12:09 -0700

    Update RYALSI.py

--- RYALSI.py
+++ RYALSI.py
@@ -749,11 +749,11 @@ async def _(e):
 async def ping(e):
     if e.sender_id in SMEX_USERS:
         start = datetime.now()
-        text = "Pong!"
+        text = "🔥⛓PONG⛓ 🔥"
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"🤖 𝗣𝗼𝗻𝗴!\n`{ms}` 𝗺𝘀")
+        await event.edit(f"🔥⛓PONG⛓ 🔥\n`{ms}` 𝗺𝘀 ☠️")
 
 
 
