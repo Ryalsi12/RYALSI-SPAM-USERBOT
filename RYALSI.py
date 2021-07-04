560073fc1c66ca0818f7b914b22f876533ea9853
Ryalsi12 <86335825+Ryalsi12@users.noreply.github.com>
2021-07-04 00:54:36 -0700

    Update RYALSI.py

--- RYALSI.py
+++ RYALSI.py
@@ -753,7 +753,7 @@ async def ping(e):
         event = await e.reply(text, parse_mode=None, link_preview=None )
         end = datetime.now()
         ms = (end-start).microseconds / 1000
-        await event.edit(f"🔥⛓PONG⛓🔥\n`{ms}` 𝗺𝘀 ☠️")
+        await event.edit(f"🏴‍🔥⛓PONG⛓🔥\n`☠🚀{ms}` 𝗺𝘀 ☠️🏴‍☠️")
 
 
 
