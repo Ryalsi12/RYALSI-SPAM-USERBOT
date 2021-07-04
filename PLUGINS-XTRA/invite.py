038b63bd8f24e63f8dd3fbd956fcfa2e860ce085
LETHAL-ARMY <83568428+LETHAL-ARMY@users.noreply.github.com>
2021-07-04 07:47:17 +0530

    Update invite.py

--- PLUGINS-XTRA/invite.py
+++ PLUGINS-XTRA/invite.py
@@ -8,9 +8,8 @@ from telethon.tl import functions
 from telethon.tl.functions.channels import GetFullChannelRequest
 from telethon.tl.functions.messages import GetFullChatRequest
 
-from userbot import CMD_HELP
-from hellbot.utils import admin_cmd, edit_or_reply, sudo_cmd
-from userbot.cmdhelp import CmdHelp
+
+
 
 async def get_chatinfo(event):
     chat = event.pattern_match.group(1)
