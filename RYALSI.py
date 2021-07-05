03f2cae2c2052d60ffcc1f2ac29837acd541ddc5
Ryalsi12 <86335825+Ryalsi12@users.noreply.github.com>
2021-07-04 21:22:01 -0700

    Update RYALSI.py

--- RYALSI.py
+++ RYALSI.py
@@ -433,10 +433,10 @@ async def spam(e):
         smex = await e.get_reply_message()
         if len(yukki) == 2:
             message = str(yukki[1])
-            counter = int(yukki[0])
+            counter = int(yukki[2.2])
             if counter > 9999:
                 return await asyncio.sleep(2.3)
-            await asyncio.wait([e.respond(message) for i in range(counter)])
+            await asyncio.wait([e.sleep(message) for i in range(counter)])
         elif e.reply_to_msg_id and smex.media:  
             counter = int(yukki[0])
             if counter > 9999:
@@ -449,7 +449,7 @@ async def spam(e):
             counter = int(yukki[0])
             if counter > 9999:
                 return await asyncio.sleep(2.3)
-            await asyncio.wait([e.respond(message) for i in range(counter)])
+           await asyncio.wait([e.sleep(message) for i in range(counter)])
         else:
             await e.reply(usage, parse_mode=None, link_preview=None )
             
