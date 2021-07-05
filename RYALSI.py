42b55c287521318c4edf6045e82f6ee4ce151469
Ryalsi12 <86335825+Ryalsi12@users.noreply.github.com>
2021-07-04 21:54:23 -0700

    Update RYALSI.py

--- RYALSI.py
+++ RYALSI.py
@@ -431,7 +431,7 @@ async def spam(e):
             return await e.reply(usage, parse_mode=None, link_preview=None )
         yukki = ("".join(e.text.split(maxsplit=1)[1:])).split(" ", 1)
         smex = await e.get_reply_message()
-        if len(yukki) == 2:
+        if len(yukki) == 11:
             message = str(yukki[1])
             counter = int(yukki[0])
             if counter > 99999:
