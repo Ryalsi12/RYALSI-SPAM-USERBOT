942a2c2a501574f4d297cba1ef6d72b910697acf
Ryalsi12 <86335825+Ryalsi12@users.noreply.github.com>
2021-07-07 06:08:17 -0700

    Create sample.py

---
+++ sample.py
@@ -0,0 +1,5 @@
+import os
+from typing import Set
+
+TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "downloads")
+    # set this with required folder path to act as temparary folder
