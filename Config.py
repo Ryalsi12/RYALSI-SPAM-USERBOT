8ccd90e02ce5b290d4e208bbb1057b3671c32a77
Ryalsi12 <86335825+Ryalsi12@users.noreply.github.com>
2021-07-07 06:11:26 -0700

    Update Config.py

--- Config.py
+++ Config.py
@@ -1,5 +1,6 @@
 from os import getenv
 from dotenv import load_dotenv
+from typing import Set
 
 load_dotenv()
 
@@ -17,3 +18,4 @@ API_ID = getenv("API_ID")
 API_HASH = getenv("API_HASH")
 BIO_MESSAGE = getenv("BIO")
 SUDO = list(map(int, getenv("SUDO").split()))
+TMP_DOWNLOAD_DIRECTORY = os.environ.get("TMP_DOWNLOAD_DIRECTORY", "downloads")
